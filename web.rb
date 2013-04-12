ec4f70da12d729050f57de74ed71214a480e47e5
Anton Dmitrijev <anton.dmitrijev@elisa.ee>
2013-04-12 11:19:53 +0300

    Allow to get records, not only counts.

--- web.rb
+++ web.rb
@@ -4,15 +4,22 @@ require 'sinatra'
 require File.expand_path(File.dirname(__FILE__) + '/cars.rb')
 
 get('/car_count/*') do
-  criteria = to_params(params[:splat].to_s)
-  return 404 unless valid?(criteria)
-  car_count(criteria).to_s
+  validate_params_and_execute(Proc.new {|criteria| car_count(criteria).to_s})
+end
+
+get('/cars/*') do
+  validate_params_and_execute(Proc.new {|criteria| query(criteria).map {|car| car.values.join(' ')}.join("\n")})
 end
 
 private
+def validate_params_and_execute(callback)
+  criteria = to_params(params[:splat].to_s)
+  return 404 unless valid?(criteria)
+  callback.call(criteria)
+end
 
-  def to_params(criteria)
-    pairs = criteria.split('/')
-    pairs.pop if pairs.size.odd?
-    Hash[*pairs]
-  end
+def to_params(criteria)
+  pairs = criteria.split('/')
+  pairs.pop if pairs.size.odd?
+  Hash[*pairs]
+end
